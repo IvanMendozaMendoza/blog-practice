6508e70b4f3a836f0e66c11600f8b1d8846b4e48
ivanmendozamendoza <ivan@LAPTOP-7KJ4UF1L.localdomain>
2025-06-27 10:20:12 -0600

    enhancing rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,8 +1,12 @@
 import rss from "@astrojs/rss"
 import { type APIRoute } from "astro";
 import { getCollection } from "astro:content";
+import MarkdownIt from "markdown-it";
+import sanitizeHtml from "sanitize-html"
 
-export const GET: APIRoute = async ({params, request, site})=> {
+const parser = new MarkdownIt()
+
+export const GET: APIRoute = async ({ params, request, site }) => {
     const blogCollection = await getCollection('blog');
 
     return rss({
@@ -10,11 +14,25 @@ export const GET: APIRoute = async ({params, request, site})=> {
         title: "The bieck's blog",
         description: "a simple practice blog",
         site: site ?? 'https://bieck-astro-blog.netlify.app',
-        items: blogCollection.map(({data, slug})=> ({
+        items: blogCollection.map(({ data, slug, body }) => ({
             title: data.title,
             description: data.description,
+            xmlns: {
+                media: 'http://search.yahoo.com/mrss/',
+              },              
             pubDate: data.date,
-            link: `/posts/${slug}`
+            link: `/posts/${slug}`,
+            content: sanitizeHtml(parser.render(body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+            }),
+
+            customData: `<media:content
+             type="image/${data.image?.format === 'jpg' ? 'jpeg' : 'png'}"
+             width="${data.image?.width}"
+             height="${data.image?.height}"
+             medium="image"
+             url="${(site ?? 'https://bieck-astro-blog.netlify.app') + (data.image?.src ?? '')}" />
+             `,
         })),
         customData: `<language>es-mx</language>`
     })
